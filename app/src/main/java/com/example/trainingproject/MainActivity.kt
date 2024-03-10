31e2be7fdc5f5c493cfa1e25b1efb98ccfd81ffc
GhenaMasri <ghenama77@gmail.com>
2024-03-10 15:55:38 +0200

    Initial Card Shape

--- app/src/main/java/com/example/trainingproject/MainActivity.kt
+++ app/src/main/java/com/example/trainingproject/MainActivity.kt
@@ -18,12 +18,6 @@ class MainActivity : ComponentActivity() {
         setContent {
             TrainingProjectTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
             }
         }
     }
