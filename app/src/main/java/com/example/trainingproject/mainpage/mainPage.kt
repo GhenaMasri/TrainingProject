852d6c00b0bbb924c8fcc3e103fbdb85a6d2b1df
GhenaMasri <ghenama77@gmail.com>
2024-03-13 13:59:50 +0200

    Adding Scaffold

--- app/src/main/java/com/example/trainingproject/mainpage/mainPage.kt
+++ app/src/main/java/com/example/trainingproject/mainpage/mainPage.kt
@@ -1,2 +1,17 @@
 package com.example.trainingproject.mainpage
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun mainPage(modifier: Modifier = Modifier){
+    Scaffold(modifier = modifier, bottomBar = {}
+    ) {paddingValues ->
+        Column(modifier = Modifier.padding(paddingValues)){}
+    }
+}
