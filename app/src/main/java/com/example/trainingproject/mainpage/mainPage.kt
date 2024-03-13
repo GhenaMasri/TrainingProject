3485a9aacf740fc9c00af9f91799020628fcc723
GhenaMasri <ghenama77@gmail.com>
2024-03-13 14:10:40 +0200

    Adding bottom nav bar and a card

--- app/src/main/java/com/example/trainingproject/mainpage/mainPage.kt
+++ app/src/main/java/com/example/trainingproject/mainpage/mainPage.kt
@@ -6,12 +6,26 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.trainingproject.cards.MainCard
+import com.example.trainingproject.navigation.myBottomNavBar
+import com.example.trainingproject.ui.theme.TrainingProjectTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun mainPage(modifier: Modifier = Modifier){
-    Scaffold(modifier = modifier, bottomBar = {}
+    Scaffold(modifier = modifier, bottomBar = { myBottomNavBar()}
     ) {paddingValues ->
-        Column(modifier = Modifier.padding(paddingValues)){}
+        Column(modifier = Modifier.padding(paddingValues)){
+           MainCard()
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun mainPagePreview(){
+    TrainingProjectTheme {
+        mainPage()
     }
 }
